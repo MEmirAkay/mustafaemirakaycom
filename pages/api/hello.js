e2079ca2f995f333e0e66327c55dbfff681afbe7
Emir Akay <emirakay073@gmail.com>
2022-06-14 22:47:02 +0300

    mongodb connection and fetch data

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectToDatabase } from "../../util/mongodb";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const { db } = await connectToDatabase();
+
+  const data = await db.collection("article").find({}).toArray();
+  console.log(data);
+  res.status(200).json(data);
 }
