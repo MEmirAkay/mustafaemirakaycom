cffebb52d372e46b0b64b11349997e9e333e77c9
Emir Akay <emirakay073@gmail.com>
2022-06-23 12:58:19 +0300

    display article by id

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,38 +1,36 @@
-
 import { Router, useRouter } from "next/router";
 import { server } from "../../../config";
 
-const article = ({article}) => {
+const article = ({ article }) => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <div>
-    <p>article _id: {id}</p>
-    <p>title: {article.header} </p>
-    <p>content : {article.content}</p>
-    
-  
-  
-  </div>;
+  return (
+    <div>
+      <p>article _id: {id}</p>
+      <p>author : {article.name}</p>
+      <p>date : {article.date}</p>
+      <p>title: {article.header} </p>
+      <p>content : {article.content}</p>
+    </div>
+  );
 };
 
 export const getServerSideProps = async (context) => {
-  const res = await fetch(`${server}/api/hello/${context.params.id}`, { method: "GET" });
+  const res = await fetch(`${server}/api/articles/${context.params.id}`, {
+    method: "GET",
+  });
   try {
     const article = await res.json();
+    return {
+      props: {
+        article,
+      },
+    };
   } catch (err) {
-    // 👇️ This runs
-    console.log('Error: ', err.message);
+    console.log("Error: ", err.message);
+    return err.message;
   }
-  
-
-  
-
-  return {
-    props: {
-      article,
-    },
-  };
 };
 
 export default article;
