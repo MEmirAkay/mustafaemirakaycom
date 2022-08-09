c6d581ac67dad5e7940d5ce6643a4002e10bb9d1
Emir Akay <emirakay073@gmail.com>
2022-08-09 09:24:37 +0300

    commit

--- pages/api/articles/index.js
+++ pages/api/articles/index.js
@@ -1,6 +1,6 @@
 import { connectToDatabase } from "../../../util/mongodb";
 
-export default async function handler(res, res) {
+export default async function handler(req, res) {
   const { db } = await connectToDatabase();
   const data = await db.collection("article").find({}).toArray();
  res.status(200).json(data)
