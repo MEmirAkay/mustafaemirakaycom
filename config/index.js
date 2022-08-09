3d6be52a14fe556e7330a234d8869c06a3aef71b
Emir Akay <emirakay073@gmail.com>
2022-08-09 09:39:31 +0300

    commit

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://www.mustafaemirakay.com'
+export const server = dev ? 'http://localhost:3000' : 'https://mustafaemirakaycom.vercel.app'
